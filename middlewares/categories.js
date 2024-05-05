6a867498c990403154bd2b1195b071c2c2dfeb1e
Winter010 <146598719+Winter010@users.noreply.github.com>
2024-05-05 20:24:08 +1000

    added functions for creating users, games, categories

--- middlewares/categories.js
+++ middlewares/categories.js
@@ -1,8 +1,22 @@
-const categories = require('../models/category');
+const categories = require("../models/category");
 
 const findAllCategories = async (req, res, next) => {
-  req.categoriesArray = await categories.find({});
-  next();
-}
+	req.categoriesArray = await categories.find({});
+	next();
+};
 
-module.exports = findAllCategories; 
+const createCategory = async (req, res, next) => {
+	console.log("POST /categories");
+	try {
+		console.log(req.body);
+		req.category = await categories.create(req.body);
+		next();
+	} catch (error) {
+		res.setHeader("Content-Type", "application/json");
+		res
+			.status(400)
+			.send(JSON.stringify({ message: "Error creating category" }));
+	}
+};
+
+module.exports = { findAllCategories, createCategory };
