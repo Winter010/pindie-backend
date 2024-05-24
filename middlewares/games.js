cec8da26de73a75fe8c0842b6b6bbc9d4febaed9
Winter010 <146598719+Winter010@users.noreply.github.com>
2024-05-24 17:18:36 +1000

    fix checkEmptyFields and checkIfCategoriesAvaliable functions

--- middlewares/games.js
+++ middlewares/games.js
@@ -61,6 +61,11 @@ const deleteGame = async (req, res, next) => {
 };
 
 const checkEmptyFields = async (req, res, next) => {
+	if (req.isVoteRequest) {
+		next();
+		return;
+	}
+
 	if (
 		!req.body.title ||
 		!req.body.description ||
@@ -93,6 +98,11 @@ const checkIfUsersAreSafe = async (req, res, next) => {
 	}
 };
 const checkIfCategoriesAvaliable = async (req, res, next) => {
+	if (req.isVoteRequest) {
+		next();
+		return;
+	}
+
 	if (!req.body.categories || req.body.categories.length === 0) {
 		res.setHeader("Content-Type", "application/json");
 		res
